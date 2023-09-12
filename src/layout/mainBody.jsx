fc54852c949465e97eefde354e294d923fa660e4
roshan0099 <roshan09ka@gmail.com>
2023-09-12 14:27:08 +0530

    updated the broken links

--- src/layout/mainBody.jsx
+++ src/layout/mainBody.jsx
@@ -51,7 +51,7 @@ export function MainBody() {
                     <div className="cols">
                         <div className="col" id="1"    >
                             <h1>Easedementia</h1>
-                            <a href="https://easedementia.com/"><img src="/github.png" alt="" height="90px" width="90px" /></a>
+                            <img src="/github.png" alt="" height="90px" width="90px" />
                            <p>
                             A web application for a start-up
                             that focuses on the well being of
@@ -61,7 +61,7 @@ export function MainBody() {
 
                         <div className="col" id="2"    >
                             <h1> Confab</h1>
-                            <a href="https://github.com/roshan0099/spaceWeb"><img src="/github.png" alt="" height="90px" width="90px" /></a>
+                            <img src="/github.png" alt="" height="90px" width="90px" />
                             <p>
                                 Trying to get my hands on socketio and its different possibilites by creating
                                 an application where you can cht as well as draw with other people real time (work in progress).
@@ -73,7 +73,7 @@ export function MainBody() {
     
                         <div className="col" id="3"    >
                             <h1> Musik </h1>
-                            <a href="https://github.com/roshan0099/musik-nodejs"><img src="/github.png" alt="" height="90px" width="90px" /></a>
+                            <img src="/github.png" alt="" height="90px" width="90px" />
                             <p>
                                 A web application uses spotify api and web-scraping to get video id from the
                                 youtube
@@ -82,7 +82,7 @@ export function MainBody() {
                         </div>
                         <div className="col" id = "4"    >
                             <h1> SpaceWeb</h1>
-                            <a href="https://roshan0099.github.io/spaceWeb/"><img src="/github.png" alt="" height="90px" width="90px" /></a>
+                            <img src="/github.png" alt="" height="90px" width="90px" />
                             <p>
                                 Space is a simple yet a complex esolang with just 6 symbols. It’s one of
                                 the smallest interpreted programming language out there
